0807cc26300f1983db5018b7a683e804ce615d62
CarolinaCasas <casasc9@uabc.edu.mx>
2021-07-22 17:18:23 -0700

    trabajandoImagenes

--- src/data.js
+++ src/data.js
@@ -1,9 +1,26 @@
 // estas funciones son de ejemplo
+const manipulacionData ={
 
-export const example = () => {
+
+/*const imagenes= ()=>{
+  document.getElementById("img1").src="image2.jpg";
+}*/
+
+/*function cambiarImagenJS(){
+  for (let = 0; < array.length;++) {
+    const element = array[
+    ];
+    
+  }
+}*/
+  
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+}
+
+export default manipulacionData;
