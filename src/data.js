fe03a885c7c9e4e7b9033d6e4d0417493675497d
nayolv <85193697+nayolv@users.noreply.github.com>
2021-07-27 16:44:53 -0500

    Filtros nombre (#6)
    * cambios en css y maquetado pag principal
    
    * ciclo foreach
    
    * trabajandoImagenes
    
    * nuevos cambios
    
    * filtros problema con la condicional
    
    * actualizacion al buscador
    
    * prueba de cambios
    
    * Acomodando el codigo
    
    * actualizacion filtro nombre
    
    Co-authored-by: CarolinaCasas <casasc9@uabc.edu.mx>

--- src/data.js
+++ src/data.js
@@ -1,9 +1,40 @@
-// estas funciones son de ejemplo
+const datos = {
+/*
+searching:function(dataImportada, tarjetaInfo, tipoP, resistenciaP, debilidadesP){
 
-export const example = () => {
-  return 'example';
+  dataImportada.forEach(poke => {
+
+    
+    const filtradoPorNombre = poke.name;
+    const imgSelec = poke.img;
+    const busquedaTipo = "TIPO: " + poke.type;
+    const buesquedaResistencia = " RESISTENCIA: " + poke.resistant;
+    const busqueaDebilidades = "DEBILIDAD: " + poke.weaknesses;
+
+    if (filtradoPorNombre === tarjetaInfo) {
+
+      tipoP.textContent = busquedaTipo.replaceAll(",", " ");
+      resistenciaP.textContent = buesquedaResistencia.replaceAll(",", " ");
+      debilidadesP.textContent = busqueaDebilidades.replaceAll(",", " ");
+
+      document.getElementById("imagen").src = imgSelec;
+
+    }
+
+  });
+},
+
+
+/*
+dropdown:function(dataImportada, elementoSeleccionado, tipoP){
+
+  const listaPokeTipo = dataImportada.filter(poke => elementoSeleccionado == poke.type);
+  const names = listaPokeTipo.map(nombres => nombres.name);
+
+  tipoP.textContent = names;
+
+}*/
+
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export default datos
--- src/main.js
+++ src/main.js
@@ -1,6 +1,123 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import datos from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+const dataImportada = data.pokemon;
+
+
+document.getElementById("btnSearch").addEventListener("click", function () {
+
+  const searchUser = document.getElementById("buscador").value.toLowerCase();
+  const buscadorFuncional = dataImportada.filter(poke => poke.name == searchUser);
+
+ 
+  buscadorFuncional.forEach(item =>{
+
+  const imgSelec = item.img;
+  document.getElementById("imagen").src = imgSelec;
+
+
+  const listaTipo = document.getElementById("typePokemon");
+  const liTipo = document.createElement("li");
+  
+  const tipos = item.type;
+  liTipo.textContent = tipos;
+  listaTipo.appendChild(liTipo);
+  
+  const listaResistencia = document.getElementById("resistantPokemon");
+  const liResistencia = document.createElement("li");
+  
+  const resistencia = item.resistant;
+  
+  liResistencia.textContent = resistencia;
+  listaResistencia.appendChild(liResistencia);
+  
+  const listaDebilidad = document.getElementById("weaknessesPokemon");
+  const liDebilidad = document.createElement("li");
+
+  const debilidad = item.weaknesses;
+  liDebilidad.textContent = debilidad;
+  listaDebilidad.appendChild(liDebilidad);
+  
+  })
+})
+
+//MENU DESPLEGABLE
+
+document.getElementById("menuDesplegable").addEventListener("change", function () {
+
+
+  const elementoSeleccionado = document.getElementById("menuDesplegable").value;
+  const menuDesp = dataImportada.filter(poke => poke.type == elementoSeleccionado);
+  const names = menuDesp.map(nombres => nombres.name);
+
+  names.forEach(item => {
+    console.log(item)
+
+    const creandoLista = document.getElementById("lista");
+    const creandoLi = document.createElement("li");
+
+    creandoLi.textContent = item;
+
+    creandoLista.appendChild(creandoLi);
+
+
+  });
+
+
+});
+
+
+
+
+
+
+//console.log(creandoLista)
+
+
+
+
+
+//---------------------------pruebas-----------------------------------------------------------
+
+//document.querySelector('#prueba').innerHTML = info.pokemon[0]["about"];
+
+/*
+console.log(datos);
+
+let cambio = document.getElementById('cambioTarj');
+cambio.style.visibility = 'hidden';
+
+
+document.querySelector("#element").addEventListener("click", function () {
+ const ocultarTarj = document.getElementById('element');
+ ocultarTarj.style.visibility = 'hidden';
+
+let tipos = document.getElementById('types');
+
+tipos.style.visibility = 'visible';
+
+
+
+*/
+/*;
+
+
+document.getElementById("tiposPokemon").addEventListener("change",function(){
+
+ let valorSelect = elementoSeleccionado.value;*/
+
+
+ //info.pokemon.forEach(poke=>{})
+
+
+//let prueba = dataImportada.map(tipos => tipos.type);
+
+
+
+/*const filtradoElectrico = dataImportada.filter(tipos => tipos.type == 'fire')
+
+
+console.log(filtradoElectrico)*/
+
+
