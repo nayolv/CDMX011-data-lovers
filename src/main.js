60647bdfe2ed636a2f6500809468da827703c05c
nayolv <85193697+nayolv@users.noreply.github.com>
2021-08-02 09:15:50 -0500

    Trabajando responsive (#9)
    * cambios en css y maquetado pag principal
    
    * ciclo foreach
    
    * trabajandoImagenes
    
    * nuevos cambios
    
    * filtros problema con la condicional
    
    * actualizacion al buscador
    
    * prueba de cambios
    
    * Acomodando el codigo
    
    * actualizacion filtro nombre
    
    * actualizaciones para trabajar css
    
    * trabajando css
    
    * nuevos cambios Css
    
    Co-authored-by: CarolinaCasas <casasc9@uabc.edu.mx>

--- src/main.js
+++ src/main.js
@@ -1,24 +1,32 @@
 import datos from './data.js';
 import data from './data/pokemon/pokemon.js';
-//import data from './data/pokemon/pokemon.js';
 
 const dataImportada = data.pokemon;
+
 //Pokemon en pantalla inicial
-window.onload= function(){
+window.onload = function () {
   pokePantallaInicial();
 };
 
-function pokePantallaInicial(){
+function pokePantallaInicial() {
   dataImportada.forEach(item => {
-    //div para las tarjetas
+    //CREANDO div para las tarjetas
     const contenedorInicio = document.getElementById("pokeInicio");
     const contenedorImgyNombre = document.createElement("div");
-    contenedorImgyNombre.setAttribute("id","pokeTarjeta");
-    contenedorImgyNombre.setAttribute("class","claseTarjeta");
-  
+    contenedorImgyNombre.setAttribute("id", "pokeTarjeta");
+    contenedorImgyNombre.setAttribute("class", "claseTarjeta");
+
     contenedorInicio.appendChild(contenedorImgyNombre);
 
-    //imagen pokemon
+    //CONDICIONAL PARA MOSTRAR SOLO N° POKEMONES
+    
+    const liNum = document.createElement("p");
+    const num = item.num;
+    if (num <= "030") {
+       liNum.textContent = "N° " + num;
+    contenedorImgyNombre.appendChild(liNum);
+    console.log(num);
+    
     const images = item.img;
     const elementoImg = document.createElement("img");
     elementoImg.src = images;
@@ -29,33 +37,31 @@ function pokePantallaInicial(){
     const nombre = item.name;
     liTipo.textContent = nombre;
     contenedorImgyNombre.appendChild(liTipo);
-  
-});
+      
+    }
+    //POPUP ABOUT 
+    let modal = document.getElementById("miModal");
+    let flex = document.getElementById("flex");
+    let cerrar = document.getElementById("close");
+
+    contenedorImgyNombre.addEventListener("click", function () {
+      modal.style.display = "block";
+      const contenedorModal = document.getElementById("informacion");
+      const acercaDe =item.about;
+      contenedorModal.textContent = acercaDe;
+
+    });
+    cerrar.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (e) {
+      if (e.target == flex) {
+        modal.style.display = "none";
+      }
+    });
+  });
 }
-
-/*function pokePantallaInicial(){
-  dataImportada.forEach(item => {
-    //div para las tarjetas
-    const contenedorInicio = document.getElementById("pokeInicio");
-    const contenedorImgyNombre = document.createElement("div");
-    contenedorImgyNombre.setAttribute("id","pokeTarjeta");
-    contenedorInicio.appendChild(contenedorImgyNombre);
-
-    //imagen pokemon
-    const contenedorImg = document.getElementById("pokeTarjeta");
-    const images = item.img;
-    const elementoImg = document.createElement("img");
-    elementoImg.src = images;
-    contenedorImg.appendChild(elementoImg);
-
-    //tipo pokemon
-    const liTipo = document.createElement("p");
-    const tipos = item.type;
-    liTipo.textContent = "TIPO: " + tipos;
-    contenedorImg.appendChild(liTipo);
-    
-});
-}*/
 
 //BUSCADOR
 document.getElementById("btnSearch").addEventListener("click", function () {
@@ -63,34 +69,36 @@ document.getElementById("btnSearch").addEventListener("click", function () {
   const searchUser = document.getElementById("buscador").value.toLowerCase();
   const buscadorFuncional = dataImportada.filter(poke => poke.name == searchUser);
 
+  document.getElementById("pokeInicio").style.display = "none";
+
+
   buscadorFuncional.forEach(item => {
+
     //imagen pokemon
     const imgSelec = item.img;
     document.getElementById("imagen").src = imgSelec;
-    //tipo pokemon
-    const contenedorPokeInfo = document.getElementById("info");
-    const liTipo = document.createElement("p");
-    const tipos = item.type;
-    liTipo.textContent = "TIPO: " + tipos;
-    contenedorPokeInfo.appendChild(liTipo);
-    //resistencia pokemon
-    const liResistencia = document.createElement("p");
+
+    const tipo = item.type;
+    document.getElementById("tipo").textContent = tipo;
+
     const resistencia = item.resistant;
-    liResistencia.textContent = "RESISTENCIA " + resistencia;
-    contenedorPokeInfo.appendChild(liResistencia);
-    //debilidades pokemon    
-    const liDebilidad = document.createElement("p");
+    document.getElementById("resistencia").textContent = resistencia;
+
     const debilidad = item.weaknesses;
-    liDebilidad.textContent = "DEBILIDAD " + debilidad;
-    contenedorPokeInfo.appendChild(liDebilidad);
-  })
-  
+    document.getElementById("debilidad").textContent = debilidad;
+
+    /*const contenedorInicio = document.getElementById("pokeInicio");
+    contenedorInicio.style.visibility = 'hidden';*/
+  });
+
 }),
-  //recupera el valor del select
+
+  //recupera el valor del select (MENU DESPLEGABLE)
 
   document.getElementById("menuDesplegable").addEventListener("change", function () {
     recuperarSeleccionado();
 
+    document.getElementById("pokeInicio").style.display = "none";
   });
 
 function recuperarSeleccionado() {
@@ -99,19 +107,27 @@ function recuperarSeleccionado() {
 
   //recupera imagen y nombre del objeto
   menuDesp.forEach(item => {
+    let contenedor = document.getElementById("imagenes");
 
     const images = item.img;
-    const contenedorImg = document.getElementById("imagenes");
     const elementoImg = document.createElement("img");
-
     elementoImg.src = images;
-    contenedorImg.appendChild(elementoImg);
+    contenedor.appendChild(elementoImg);
 
+    const names = document.createElement("p");
     const nombres = item.name;
-    const names = document.createElement("h3");
 
-    names.textContent = nombres;
-    contenedorImg.appendChild(names);
+    names.innerHTML = nombres;
+    contenedor.appendChild(names);
   });
 
 }
+
+document.getElementById("botonNuevaBusqueda").addEventListener("click", function () { location.reload() });
+
+/*
+cambio.style.visibility = 'hidden';
+
+cambio.style.visibility = 'hidden';*/
+
+
